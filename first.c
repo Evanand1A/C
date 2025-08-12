70f09aa4d7e1a75a3a5785d6bc4c7adab4dd4430
Evanand1A <evanajish960@gmail.com>
2025-08-12 10:08:05 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    // 1 Add two numbers
+    int a,b;
+    printf("Ënter two numbers to be added:");
+    scanf("%d %d",&a,&b);
+    printf("The sum of the given two numbers is:%d %d\n",a+b);
+}

7d4234f2e96d545bd5b77b518559f3577dfb2379
Evanand1A <evanajish960@gmail.com>
2025-08-18 23:26:35 +0530

    Create Q9.c

---
+++ Q9.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Q9. Convert rupees into dollars (1$ = 48 Rs)
+int main(){
+    float rupees;
+    printf("Enter amount in rupees: ");
+    scanf("%f",&rupees);
+    printf("Dollars = %.2f\n",rupees/48);
+    return 0;
+}

423f050cef1cba09032a15e092ad8c56ffbf80ef
Evanand1A <evanajish960@gmail.com>
2025-08-18 23:23:59 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Q3. Multiply two numbers
+int main(){
+    int a,b;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("Product = %d\n",a*b);
+    return 0;
+}

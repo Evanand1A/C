eda89d843c2dbd8782c9012cd81c36c2a112d775
Evanand1A <evanajish960@gmail.com>
2025-08-18 23:21:40 +0530

    Add files via upload

---
+++ First.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Q2. Subtract two numbers
+int main(){
+    int a,b;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("Difference = %d\n",a-b);
+    return 0;
+}
+

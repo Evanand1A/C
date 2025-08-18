065f51e5113608ebca9cb0ba0e123c9bd6ed0cb8
Evanand1A <evanajish960@gmail.com>
2025-08-18 23:25:21 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// Q5. Add, subtract, multiply, and divide two numbers
+int main(){
+    int a,b;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("Sum = %d\n",a+b);
+    printf("Difference = %d\n",a-b);
+    printf("Product = %d\n",a*b);
+    if(b!=0){
+        printf("Quotient = %d\n",a/b);
+    }else{
+        printf("Division not possible (denominator is zero).\n");
+    }
+    return 0;
+}

d7fdd433052ed2b1bf911517d49a05dda2bd3630
Evanand1A <evanajish960@gmail.com>
2025-08-18 23:26:00 +0530

    Create Q7.c

---
+++ Q7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Q7. Convert hours into minutes
+int main(){
+    int hours;
+    printf("Enter time in hours: ");
+    scanf("%d",&hours);
+    printf("Minutes = %d\n",hours*60);
+    return 0;
+}

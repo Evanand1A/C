34cecaba482a306c66c54a3c371bc6781828fc34
Evanand1A <evanajish960@gmail.com>
2025-08-18 23:25:40 +0530

    Create Q6.c

---
+++ Q6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Q6. Convert minutes into hours
+int main(){
+    int minutes;
+    printf("Enter time in minutes: ");
+    scanf("%d",&minutes);
+    printf("Hours = %d\n",minutes/60);
+    return 0;
+}

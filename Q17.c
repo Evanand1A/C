30a50e41e6bed15ba191f43e32dec9df0c7172fe
Evan Ajish <evanajish960@gmail.com>
2025-08-22 08:36:16 +0530

    Create Q17.c

---
+++ Q17.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// Q17. Calculate area and perimeter of a square
+int main(){
+    int l;
+    printf("Enter side length of square: ");
+    scanf("%d",&l);
+    printf("Area = %d\n",l*l);
+    printf("Perimeter = %d\n",4*l);
+    return 0;
+}

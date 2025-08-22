651ae5f9b1155a9b661f6a74a968b2d186df66b7
Evan Ajish <evanajish960@gmail.com>
2025-08-22 08:37:01 +0530

    Create Q19.c

---
+++ Q19.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Q19. Calculate area of a circle: A = 22/7 × R × R
+int main(){
+    float r;
+    printf("Enter radius: ");
+    scanf("%f",&r);
+    printf("Area = %.2f\n",(22.0/7.0)*r*r);
+    return 0;
+}

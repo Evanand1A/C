f5f984d26936bef32664f0a852570c2be9b7bc7c
Evan Ajish <evanajish960@gmail.com>
2025-08-22 08:34:13 +0530

    Create Q13.c

---
+++ Q13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Q13. Convert bytes into KB, MB, and GB
+int main(){
+    long bytes;
+    printf("Enter size in bytes: ");
+    scanf("%ld",&bytes);
+    printf("Kilobytes = %.2f\n",bytes/1024.0);
+    printf("Megabytes = %.2f\n",bytes/(1024.0*1024));
+    printf("Gigabytes = %.2f\n",bytes/(1024.0*1024*1024));
+    return 0;
+}

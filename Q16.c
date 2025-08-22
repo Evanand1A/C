7249e8ee6b0cbf46ad0ba84df41566a1a897eb94
Evan Ajish <evanajish960@gmail.com>
2025-08-22 08:35:55 +0530

    Create Q16.c

---
+++ Q16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Q16. Calculate interest where I = PRT / 100
+int main(){
+    float p,r,t;
+    printf("Enter principal: ");
+    scanf("%f",&p);
+    printf("Enter rate of interest: ");
+    scanf("%f",&r);
+    printf("Enter time (in years): ");
+    scanf("%f",&t);
+    printf("Interest = %.2f\n",(p*r*t)/100);
+    return 0;
+}

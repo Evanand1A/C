8b1acb6f53cd86977c3413ee7211aed989fa7a9c
Evan Ajish <evanajish960@gmail.com>
2025-08-22 08:35:05 +0530

    Create Q15.c

---
+++ Q15.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Q15. Convert Fahrenheit into Celsius: C = 5/9 × (F − 32)
+int main(){
+    float f;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f",&f);
+    printf("Celsius = %.2f\n",(5.0/9.0)*(f-32));
+    return 0;
+}
